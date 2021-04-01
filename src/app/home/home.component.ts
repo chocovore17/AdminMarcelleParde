3dfa4691ac1a1fc284a2acba25f996f76abb7d91
chocovore17 <47193844+chocovore17@users.noreply.github.com>
2021-04-01 10:55:55 +0100

    prints student name

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,21 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from "@angular/fire/firestore";
 import { FormControl, FormGroup } from "@Angular/forms";
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent implements OnInit {
+  public student: Student;
   show = true;
   myArray: any[] = [];
   message:string;
   single:any;
+  tmp:string;
   secondForm = new FormGroup({ valueToGet: new FormControl('') })
   form = new FormGroup({
     newValue: new FormControl('')
 })
+
   constructor(private firestore: AngularFirestore) {}
 
   ngOnInit(): void {
@@ -29,25 +32,30 @@ export class HomeComponent implements OnInit {
   });
 
   }
+  
 
-
-  onQuery(classe:string, prenom:string) {
-    if (!prenom) {
-      this.message = 'Entrez un prénom à rechercher';
+  onQuery(classe:string, nom:string) {
+    if (!nom) {
+      this.message = 'Entrez un nom de famille à rechercher';
       this.single = null;
     }if (!classe) {
       this.message = 'Entrez une classe svp';
       this.single = null;
     } else {
-      this.firestore.collection(classe, ref => ref.where("field", "==",prenom)).get()
+      // var citiesRef = this.firestore.collection(classe);
+      this.firestore.collection(classe, ref => ref.where("nom", "==",nom)).get()
         .subscribe(ss => {
           if (ss.docs.length === 0) {
             this.message = "Cet élève n'a pas été trouvé. Merci d'essayer à nouveau ou de le contacter.";
             this.single = null;
           } else {
             ss.docs.forEach(doc => {
-              this.message = '';
+              this.message = 'elève trouvé!';
               this.single = doc.data();
+              console.log("============================================");
+              console.log(this.single);
+              this.tmp = this.single.nom;
+              // this.tmp = this.single.toString();
             })
           }
         })
