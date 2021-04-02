752f66cdc98d0a8a37e8ef6c505a724d23026445
chocovore17 <47193844+chocovore17@users.noreply.github.com>
2021-04-02 11:57:11 +0100

    FIXED TIMEOUT§§

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,7 +14,7 @@ import { COVIDData } from './COVIDdata';
 export class HomeComponent implements OnInit {
   curr_covidData = [];
   CasContacts = [];
-  public show:boolean = false;
+  public show: boolean = false;
   jstoday = '';
   today = new Date();
   message: string;
@@ -26,13 +26,13 @@ export class HomeComponent implements OnInit {
     this.jstoday = formatDate(this.today, 'dd-MM-yyyy', 'en-US');
   }
 
-  ngOnInit(): void {}
-  showResults(){
+  ngOnInit(): void { }
+  showResults() {
     this.show = true;
   }
-  createDateArray(jours : number){
-    var datearray : string[]=[];
-    if(jours < 1) jours = 7;
+  createDateArray(jours: number) {
+    var datearray: string[] = [];
+    if (jours < 1) jours = 7;
     for (var i = 0; i < jours; i++) {
       var dayexact = parseInt(this.jstoday.substring(0, 2));
       var monthexact = parseInt(this.jstoday.substring(3, 5));
@@ -55,73 +55,82 @@ export class HomeComponent implements OnInit {
     }
     return datearray;
   }
-  findTablesAndDates(nom, datearray){
-    var listtables = [];
-    var listdates= [];
-    this.firestore.collection("MarcelleParde").get()
-    .subscribe(ss => {
-      // issue with the date idk why
-      if (ss.docs.length === 0) {
-        this.message = "Le lycée n'existe pas dans la database.";
-      } else {
-        ss.docs.forEach(doc => {
-          var keyvalue = doc.data();
-          if (nom in keyvalue&& datearray.includes(doc.id)) {
-            var all_covid_data : COVIDData = new COVIDData(keyvalue[nom], doc.id);
-            console.log("all covid data", all_covid_data);
-            this.curr_covidData.push(all_covid_data);
-            listtables.push(keyvalue[nom]);
-            listdates.push(doc.id);
-          }
-        })
-      }
-    });
-    listdates.forEach(item=>{
-      console.log(item);
-      console.log(item);
-    })
-  }
-
-  findPeopleAtRisk(){
-    this.curr_covidData.forEach(item =>{
-      var cascontactscejour = [];
-      cascontactscejour.push(item.date);
-       this.firestore.collection("MarcelleParde").doc(item.date).collection(item.table).get()
-        .subscribe((ss) => {
-          ss.docs.forEach((doc) => {
-             if(typeof doc.data().nom !== "undefined" && typeof doc.data().classe !== "undefined"){
-              cascontactscejour.push(doc.data().prenom+ ' ' + doc.data().nom+ ', classe : '+ doc.data().classe+'\n');
-             }
-          })
-          this.CasContacts.push(cascontactscejour);
-        })
-       });
-  }
   
+  async findTablesAndDates(nom, datearray) {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve(
+          this.firestore.collection("MarcelleParde").get()
+      .subscribe(ss => {
+        // issue with the date idk why
+        if (ss.docs.length === 0) {
+          this.message = "Le lycée n'existe pas dans la database.";
+        } else {
+          ss.docs.forEach(doc => {
+            var keyvalue = doc.data();
+            if (nom in keyvalue && datearray.includes(doc.id)) {
+              var all_covid_data: COVIDData = new COVIDData(keyvalue[nom], doc.id);
+              console.log("all covid data", all_covid_data);
+              this.curr_covidData.push(all_covid_data);
+            }
+          })
+        }
+      })
+        );
+      }, 200);
+    });
+      }
 
-  onQuery( nom: string, jours = 7) { //default display for a week
+  async findPeopleAtRisk() {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve(
+          this.curr_covidData.forEach(item => {
+            var cascontactscejour = [];
+            cascontactscejour.push(item.date);
+            this.firestore.collection("MarcelleParde").doc(item.date).collection(item.table).get()
+              .subscribe((ss) => {
+                ss.docs.forEach((doc) => {
+                  if (typeof doc.data().nom !== "undefined" && typeof doc.data().classe !== "undefined") {
+                    cascontactscejour.push(doc.data().prenom + ' ' + doc.data().nom + ', classe : ' + doc.data().classe + '\n');
+                  }
+                })
+                this.CasContacts.push(cascontactscejour);
+              })
+          }));
+      }, 600);
+    });
+  }
+
+  async onQuery(nom: string, jours = 7) { //default display for a week
     // reset all arrays to print only data asked for 
     this.curr_covidData = [];
     if (!nom) {
       this.message = 'Entrez un nom de famille à rechercher';
-    }  else {
+    } else {
       var datearray = this.createDateArray(jours);
-      console.log("datearray", datearray);
-      
-      // 1: search  and add to curr covid data for last 7 days 
-      this.findTablesAndDates(nom, datearray);
-
+      // var num = await this.findTablesAndDates(nom, datearray);
+      console.log("find tables and dates should be done first ");
       this.CasContacts = [];
-      console.log(this.curr_covidData);
-      // find people who were in contact from tuple (day, table)
-      this.findPeopleAtRisk();
-      console.log("now cas contacts is", this.CasContacts);
+      this.findTablesAndDates(nom, datearray).then(value => {
+        this.findPeopleAtRisk();
+      });
+      // num = await this.findPeopleAtRisk();
+      // console.log("find people at risk should be done second ");
+
     }
 
   }
 }
 
+//  // 1: search  and add to curr covid data for last 7 days 
+//  this.findTablesAndDates(nom, datearray);
 
+//  this.CasContacts = [];
+//  console.log(this.curr_covidData);
+//  // find people who were in contact from tuple (day, table)
+//  this.findPeopleAtRisk();
+//  console.log("now cas contacts is", this.CasContacts);
 // TO : 
 //  fix double press button 
 // looks
